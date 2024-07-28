0edd2069402ac53192cb7980f8452ccb88fed5a3
teodorstupnicki <teodor.stupnicki@gmail.com>
2024-07-28 16:39:20 +0200

    Added custom raffle error

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -2,14 +2,20 @@
 pragma solidity 0.8.19;
 
 contract Raffle {
+  /* Errors */
+  error Raffle__SendMoreToEnterRaffle();
+  
   uint256 private immutable i_entranceFee;
 
   constructor(uint256 entranceFee) {
     i_entranceFee = entranceFee;
   }
 
-  function enterRaffle() public {
+  function enterRaffle() public payable {
     require(msg.value >= i_entranceFee, "Not enough ETH sent!");
+    if(msg.value < i_entranceFee) {
+      revert Raffle__SendMoreToEnterRaffle();
+    }
   }
 
   function pickWinner() public {}
