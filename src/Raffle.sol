b825e633fcde3c6c0cc71a387d886086e04cda27
teodorstupnicki <teodor.stupnicki@gmail.com>
2024-08-03 10:14:38 +0200

    Added time interval for the raffle

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -6,16 +6,21 @@ contract Raffle {
   error Raffle__SendMoreToEnterRaffle();
   
   uint256 private immutable i_entranceFee;
+  address payable[] private s_players;
+
+  /* Events */
+  event RaffleEntered(address indexed player);
 
   constructor(uint256 entranceFee) {
     i_entranceFee = entranceFee;
   }
 
   function enterRaffle() public payable {
-    require(msg.value >= i_entranceFee, "Not enough ETH sent!");
     if(msg.value < i_entranceFee) {
       revert Raffle__SendMoreToEnterRaffle();
     }
+    s_players.push(payable(msg.sender));
+    emit RaffleEntered(msg.sender);
   }
 
   function pickWinner() public {}
