38f8f61327fcbd908bd4304301f2dd41f9c8a804
teodorstupnicki <teodor.stupnicki@gmail.com>
2024-08-03 13:47:34 +0200

    Added VRFCorrdinator constructor call

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -15,11 +15,12 @@ contract Raffle is VRFConsumerBaseV2Plus {
   /* Events */
   event RaffleEntered(address indexed player);
 
-  constructor(uint256 entranceFee, uint256 interval) {
-    i_entranceFee = entranceFee;
-    i_interval = interval;
-    s_lastTimeStamp = block.timestamp;
-  }
+  constructor(uint256 entranceFee, uint256 interval, address vrfCoordinator)
+    VRFConsumerBaseV2Plus(vrfCoordinator) {
+      i_entranceFee = entranceFee;
+      i_interval = interval;
+      s_lastTimeStamp = block.timestamp;
+    }
 
   function enterRaffle() public payable {
     if(msg.value < i_entranceFee) {
@@ -29,22 +30,24 @@ contract Raffle is VRFConsumerBaseV2Plus {
     emit RaffleEntered(msg.sender);
   }
 
-  function pickWinner() public {
+  function pickWinner() external {
     if ((block.timestamp - s_lastTimeStamp) > i_interval) {
       revert();
     }
-    requestId = s_vrfCoordinator.requestRandomWords(
-      VRFV2PlusClient.RandomWordsRequest({
-        keyHash: s_keyHash,
-        subId: s_subscriptionId,
-        requestConfirmations: requestConfirmations,
-        callbackGasLimit: callbackGasLimit,
-        numWords: numWords,
-        extraArgs: VRFV2PlusClient._argsToBytes(
-          // Set nativePayment to true to pay for VRF requests with Sepolia ETH instead of LINK
-          VRFV2PlusClient.ExtraArgsV1({nativePayment: false})
-        )
-      })
-    );
+    // requestId = s_vrfCoordinator.requestRandomWords(
+    //   VRFV2PlusClient.RandomWordsRequest({
+    //     keyHash: s_keyHash,
+    //     subId: s_subscriptionId,
+    //     requestConfirmations: requestConfirmations,
+    //     callbackGasLimit: callbackGasLimit,
+    //     numWords: numWords,
+    //     extraArgs: VRFV2PlusClient._argsToBytes(
+    //       // Set nativePayment to true to pay for VRF requests with Sepolia ETH instead of LINK
+    //       VRFV2PlusClient.ExtraArgsV1({nativePayment: false})
+    //     )
+    //   })
+    // );
   }
+
+  function fulfillRandomWords(uint256 requestId, uint256[] calldata randomWords) internal override {}
 }
