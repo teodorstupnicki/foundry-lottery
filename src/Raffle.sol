d060167f8cc76d6a10af8888df423a19bfcb950e
teodorstupnicki <teodor.stupnicki@gmail.com>
2024-08-03 12:27:10 +0200

    Installed Chainlink VRF contract added inheritance

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
-contract Raffle {
+import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+
+contract Raffle is VRFConsumerBaseV2Plus {
   /* Errors */
   error Raffle__SendMoreToEnterRaffle();
   
@@ -28,8 +30,21 @@ contract Raffle {
   }
 
   function pickWinner() public {
-    if (block.timestamp - s_lastTimeStamp > i_interval) {
+    if ((block.timestamp - s_lastTimeStamp) > i_interval) {
       revert();
     }
+    requestId = s_vrfCoordinator.requestRandomWords(
+      VRFV2PlusClient.RandomWordsRequest({
+        keyHash: s_keyHash,
+        subId: s_subscriptionId,
+        requestConfirmations: requestConfirmations,
+        callbackGasLimit: callbackGasLimit,
+        numWords: numWords,
+        extraArgs: VRFV2PlusClient._argsToBytes(
+          // Set nativePayment to true to pay for VRF requests with Sepolia ETH instead of LINK
+          VRFV2PlusClient.ExtraArgsV1({nativePayment: false})
+        )
+      })
+    );
   }
 }
