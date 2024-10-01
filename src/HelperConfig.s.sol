c74076cfdb7418afe2b18af676085c4a2b80ff86
teodorstupnicki <teodor.stupnicki@gmail.com>
2024-10-02 00:37:59 +0200

    Created NetworkConfig struct

--- src/HelperConfig.s.sol
+++ src/HelperConfig.s.sol
@@ -1,2 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
+
+import {Script} from "forge-std/Script.sol";
+
+contract HelperConfig is Script {
+    struct NetworkConfig {
+        uint256 subscriptionId;
+        bytes32 gasLane;
+        uint256 automationUpdateInterval;
+        uint256 raffleEntranceFee;
+        uint32 callbackGasLimit;
+        address vrfCoordinatorV2_5;
+    }
+}
