abcdd7db7d74b386bee742564a1496857bc96316
teodorstupnicki <teodor.stupnicki@gmail.com>
2024-10-02 00:41:22 +0200

    Added Sepolia network config

--- src/HelperConfig.s.sol
+++ src/HelperConfig.s.sol
@@ -12,4 +12,20 @@ contract HelperConfig is Script {
         uint32 callbackGasLimit;
         address vrfCoordinatorV2_5;
     }
+
+    NetworkConfig public localNetworkConfig;
+    mapping(uint256 chainId => NetworkConfig) public networkConfigs;
+
+    function getSepoliaEthConfig() public pure returns (NetworkConfig memory sepoliaNetworkConfig) {
+        sepoliaNetworkConfig = NetworkConfig({
+            subscriptionId: 0, // If left as 0, our scripts will create one!
+            gasLane: 0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae,
+            automationUpdateInterval: 30, // 30 seconds
+            raffleEntranceFee: 0.01 ether,
+            callbackGasLimit: 500000, // 500,000 gas
+            vrfCoordinatorV2_5: 0x9DdfaCa8183c41ad55329BdeeD9F6A8d53168B1B,
+            link: 0x779877A7B0D9E8603169DdbD7836e478b4624789,
+            account: 0x643315C9Be056cDEA171F4e7b2222a4ddaB9F88D
+        });
+    }
 }
