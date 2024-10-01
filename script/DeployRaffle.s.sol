ebe9fe39f33f267743350488dc9511c7317c7df0
teodorstupnicki <teodor.stupnicki@gmail.com>
2024-10-01 23:54:49 +0200

    Initialized HelperConfig

--- script/DeployRaffle.s.sol
+++ script/DeployRaffle.s.sol
@@ -2,7 +2,9 @@
 pragma solidity ^0.8.19;
 
 import {Script} from "forge-std/Script.sol";
+import {Raffle} from "src/Raffle.sol";
 
 contract DeployRaffle is Script {
+  function run() public {}
   
 }
