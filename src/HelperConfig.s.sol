aae5c3b0b5448e8f3c6c59567d6505ac74f90d9f
teodorstupnicki <teodor.stupnicki@gmail.com>
2024-10-02 23:29:45 +0200

    Added CodeConstants abstract contract

--- src/HelperConfig.s.sol
+++ src/HelperConfig.s.sol
@@ -3,7 +3,22 @@ pragma solidity 0.8.19;
 
 import {Script} from "forge-std/Script.sol";
 
-contract HelperConfig is Script {
+abstract contract CodeConstants {
+    uint96 public MOCK_BASE_FEE = 0.25 ether;
+    uint96 public MOCK_GAS_PRICE_LINK = 1e9;
+    // LINK / ETH price
+    int256 public MOCK_WEI_PER_UINT_LINK = 4e15;
+
+    address public FOUNDRY_DEFAULT_SENDER = 0x1804c8AB1F12E6bbf3894d4083f33e07309d1f38;
+
+    uint256 public constant ETH_SEPOLIA_CHAIN_ID = 11155111;
+    uint256 public constant ETH_MAINNET_CHAIN_ID = 1;
+    uint256 public constant LOCAL_CHAIN_ID = 31337;
+}
+
+contract HelperConfig is Script, CodeConstants {
+    error HelperConfig__InvalidChainId();
+
     struct NetworkConfig {
         uint256 subscriptionId;
         bytes32 gasLane;
@@ -15,6 +30,20 @@ contract HelperConfig is Script {
 
     NetworkConfig public localNetworkConfig;
     mapping(uint256 chainId => NetworkConfig) public networkConfigs;
+
+    constructor() {
+        networkConfigs[ETH_SEPOLIA_CHAIN_ID] = getSepoliaEthConfig();
+    }
+
+    function getConfigByChainId(uint256 chainId) public returns (NetworkConfig memory) {
+        if (networkConfigs[chainId].vrfCoordinatorV2_5 != address(0)) {
+            return networkConfigs[chainId];
+        } else if (chainId == LOCAL_CHAIN_ID) {
+            return getOrCreateAnvilEthConfig();
+        } else {
+            revert HelperConfig__InvalidChainId();
+        }
+    }
 
     function getSepoliaEthConfig() public pure returns (NetworkConfig memory sepoliaNetworkConfig) {
         sepoliaNetworkConfig = NetworkConfig({
