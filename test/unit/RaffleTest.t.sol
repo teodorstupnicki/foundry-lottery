e9811bf247fa83130bc9b8383b005a48a2b77f12
teodorstupnicki <teodor.stupnicki@gmail.com>
2024-10-07 23:49:00 +0200

    Added contract properties

--- test/unit/RaffleTest.t.sol
+++ test/unit/RaffleTest.t.sol
@@ -6,5 +6,16 @@ import {HelperConfig} from "../../script/HelperConfig.s.sol";
 import {Test} from "forge-std/Test.sol";
 
 contract RaffleTest is Test {
-    
+    Raffle public raffle;
+    HelperConfig public helperConfig;
+
+    uint256 subscriptionId;
+    bytes32 gasLane;
+    uint256 automationUpdateInterval;
+    uint256 raffleEntranceFee;
+    uint32 callbackGasLimit;
+    address vrfCoordinatorV2_5;
+
+    address public PLAYER = makeAddr("player");
+    uint256 public constant STARTING_USER_BALANCE = 10 ether;
 }
