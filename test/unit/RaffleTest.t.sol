9e27d8f0e655d0f6e957dc702bdc11af63385183
teodorstupnicki <teodor.stupnicki@gmail.com>
2024-10-07 23:46:48 +0200

    Added RaffleTest contract with imports

--- test/unit/RaffleTest.t.sol
+++ test/unit/RaffleTest.t.sol
@@ -1,4 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
+import {Raffle} from "../../src/Raffle.sol";
+import {HelperConfig} from "../../script/HelperConfig.s.sol";
+import {Test} from "forge-std/Test.sol";
 
+contract RaffleTest is Test {
+    
+}
