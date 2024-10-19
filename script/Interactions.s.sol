cfce90c4a7b13931f26ce43ee86fa5b2a18c391f
teodorstupnicki <teodor.stupnicki@gmail.com>
2024-10-19 14:11:50 +0200

    Added createSubscription function to create a VRF sub

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -1,8 +1,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import {Script} from "forge-std/Script.sol";
+import {Script, console} from "forge-std/Script.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
+import {VRFCoordinatorV2_5Mock} from "@chainlink/contracts/src/v0.8/vrf/mocks/VRFCoordinatorV2_5Mock.sol";
 
 contract CreateSubscritption is Script {
+  function createSubscriptionUsingConfig() public returns (uint256, address) {
+    HelperConfig helperConfig = new HelperConfig();
+    address vrfCoordinator = helperConfig.getConfig().vrfCoordinator;
+    
+  }
+
+  function createSubscritption(address vrfCoordinator) public returns (uint256, address) {
+    console.log("Creating subscription on chainId: ", block.chainid);
+    vm.startBroadcast(account);
+    uint256 subId = VRFCoordinatorV2_5Mock(vrfCoordinator).createSubscription();
+    vm.stopBroadcast();
+    console.log("Your subscription Id is: ", subId);
+    console.log("Please update the subscriptionId in HelperConfig.s.sol");
+    return (subId, vrfCoordinator);
+  }
   
+  function run() public {}
 }
