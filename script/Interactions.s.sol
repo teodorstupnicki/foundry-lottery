9a4c750b2b9e301c250cf02603d6881924f4d5cf
teodorstupnicki <teodor.stupnicki@gmail.com>
2024-10-26 18:24:52 +0200

    Increased VRF coordinator funding amount

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -47,7 +47,7 @@ contract FundSubscription is Script, CodeConstants {
 
     if (block.chainid == LOCAL_CHAIN_ID) {
         vm.startBroadcast();
-        VRFCoordinatorV2_5Mock(vrfCoordinatorV2_5).fundSubscription(subId, FUND_AMOUNT);
+        VRFCoordinatorV2_5Mock(vrfCoordinatorV2_5).fundSubscription(subId, FUND_AMOUNT * 100);
         vm.stopBroadcast();
     } else {
         console.log(LinkToken(link).balanceOf(msg.sender));
